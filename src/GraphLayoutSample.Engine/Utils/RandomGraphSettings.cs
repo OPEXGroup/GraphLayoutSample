97ee19b68b05f59c89443e3caddbf74971a4d2b2
Vladimir.Tyrin <b0-0b@yandex.ru>
2017-02-02 13:28:58 +0300

    Generator settings added

---
+++ src/GraphLayoutSample.Engine/Utils/RandomGraphSettings.cs
@@ -0,0 +1,34 @@
+﻿namespace GraphLayoutSample.Engine.Utils
+{
+    public class RandomGraphSettings
+    {
+        #region properties
+
+        public int NodeCount { get; set; } = DefaultNodeCount;
+
+        public int MinNodeDegree { get; set; } = DefaultMinNodeDegree;
+        public int MaxNodeDegree { get; set; } = DefaultMaxNodeDegree;
+
+        public double MinNodeWidth { get; set; } = DefaultMinNodeWidth;
+        public double MaxNodeWidth { get; set; } = DefaultMaxNodeWidth;
+
+        public double MinNodeHeight { get; set; } = DefaultMinNodeHeight;
+        public double MaxNodeHeight { get; set; } = DefaultMaxNodeHeight;
+        #endregion
+
+        #region constants
+
+        private const int DefaultNodeCount = 15;
+
+        private const int DefaultMinNodeDegree = 1;
+        private const int DefaultMaxNodeDegree = 5;
+
+        private const double DefaultMinNodeWidth = 150;
+        private const double DefaultMaxNodeWidth = 220;
+
+        private const double DefaultMinNodeHeight = 50;
+        private const double DefaultMaxNodeHeight = 100;
+
+        #endregion
+    }
+}
